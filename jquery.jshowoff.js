f4fdb563e3bd740d815f37a85aa9891c2d1169a5
Eric Johnson <mpythonite@gmail.com>
2012-09-24 22:20:47 -0300

    Update jquery.jshowoff.js
    Correct handling of title in slideLink for newer jQuery releases.

--- jquery.jshowoff.js
+++ jquery.jshowoff.js
@@ -228,7 +228,8 @@ speed :				time each slide is shown [integer, milliseconds, defaults to 3000]
 			function addSlideLinks() {
 				$wrap.append('<p class="jshowoff-slidelinks '+uniqueClass+'-slidelinks"></p>');
 				$.each(gallery, function(i, val) {
-					var linktext = $(this).attr('title') || i+1;
+					var title = $(this).attr('title')
+					var linktext = title && 0 !== title.length ? title : i+1;
 					$('<a class="jshowoff-slidelink-'+i+' '+uniqueClass+'-slidelink-'+i+'" href="#null">'+linktext+'</a>').bind('click', {index:i}, function(e){ goToAndPause(e.data.index); return false; }).appendTo('.'+uniqueClass+'-slidelinks');
 				});
 			};		
